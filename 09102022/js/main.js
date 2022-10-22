72901c81240283001781740750b6187488763dae
JekaIv <jonny22russ@gmail.com>
2022-10-22 15:56:43 +0700

    ew files

--- 09102022/js/main.js
+++ 09102022/js/main.js
@@ -92,25 +92,6 @@ scrollForm.onclick = () => {
 modalClose(success)
 modalClose(youtube)
 
-//cart-slider
-const sliderMain = document.querySelector('.slider-main');
-const sliderNav = document.querySelector('.slider-nav');
-
-let swiperNav = new Swiper(sliderNav, {
-    slidesPerView: 6,
-    spaceBetween: 10,
-    freeMode: true,
-    loop: true,
-})
-
-let swiperMain = new Swiper(sliderMain, {
-    spaceBetween: 10,
-    loop: true,
-    thumbs: {
-        swiper: swiperNav
-    }
-})
-
 
 
 
