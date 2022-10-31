090d112479c1f686eb0f728496549b7bb4fe9397
JekaIv <jonny22russ@gmail.com>
2022-10-31 10:13:21 +0700

    ew files

--- 09102022/pages/js/card.js
+++ 09102022/pages/js/card.js
@@ -1,5 +1,15 @@
 const sliderMain = document.querySelector('.slider-main');
 const sliderNav = document.querySelector('.slider-nav');
+const price = document.querySelector('.price__text');
+
+const options = {
+    style: 'currency',
+    currency: 'RUB',
+    minimumFractionDigits: 0
+};
+
+let numberFormat = new Intl.NumberFormat('ru-RU', options);
+price.textContent = numberFormat.format(+price.textContent);
 
 let swiperNav = new Swiper(sliderNav, {
     slidesPerView: 6,
@@ -11,6 +21,7 @@ let swiperNav = new Swiper(sliderNav, {
 let swiperMain = new Swiper(sliderMain, {
     spaceBetween: 10,
     loop: true,
+    autoHeight: true,
     thumbs: {
         swiper: swiperNav
     }
